676d1f363053cae8094eadd359ca4adf2c7baa5d
Tia Beiruty <tiabeiruty@gmail.com>
2023-04-21 22:07:25 +0300

    Displaying platform icons

--- game-hub/src/hooks/useGames.ts
+++ game-hub/src/hooks/useGames.ts
@@ -1,11 +1,20 @@
 import { CanceledError } from "axios";
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
+
+export interface Platform { 
+  id: number;
+  name: string;
+  slug: string;
+}
+
 export interface Game {
   id: number;
   name: string;
   background_image: string;
+  parent_platforms: { platform: Platform }[]
 }
+
 interface FetchGamesResponse {
   count: number;
   results: Game[];
@@ -13,19 +22,17 @@ interface FetchGamesResponse {
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
-  const controller = new AbortController();
   useEffect(() => {
+    const controller = new AbortController();
     apiClient
-      .get<FetchGamesResponse>("/games", { signal: controller.signal})
+      .get<FetchGamesResponse>("/games", { signal: controller.signal })
       .then((res) => setGames(res.data.results))
       .catch((err) => {
         if (err instanceof CanceledError) return;
-        setError(err.message)});
-      ;
-
+        setError(err.message)
+      });
     return () => controller.abort();
   }, []);
-  return {games, error}
+  return { games, error };
 }
-
-export default useGames
+export default useGames;
