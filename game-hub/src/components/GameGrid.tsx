63f8ce6cfc16b6edadafa9d950978f4df26ea85b
Tia Beiruty <tiabeiruty@gmail.com>
2023-04-23 22:45:37 +0300

    Increased number of skeletons

--- game-hub/src/components/GameGrid.tsx
+++ game-hub/src/components/GameGrid.tsx
@@ -9,7 +9,9 @@ interface Props {
 }
 const GameGrid = ({ selectedGenre }: Props) => {
   const { data, error, isLoading } = useGames(selectedGenre);
-  const skeletons = [1, 2, 3, 4, 5, 6];
+  const skeletons = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+  ];
 
   return (
     <>
